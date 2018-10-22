ff10a31e05bab4585a9668874f4663a39abcc250
Mickell <mickellc@hotmail.co.uk>
2018-10-22 17:15:41 +0100

    Update about article content

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,20 +55,23 @@ class Main extends React.Component {
           {close}
         </article>
 
-
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
           <p>
           I Am an Associate developer with full-stack experience. Strong knowledge of agile practices and principles and I am Professional Scrum Master 1 certified. 5 years experience working in the IT industry this includes: tech support, field engineer, finance, legacy systems  and software development.
-          Below is a list of the social aspects I like to do outside of work.
+          Below is some of the social events I like to do outside of work.
           </p>
-          <ul>
-            <li>I play for 2 football teams and play 3 times a week, I also enjoy watching football </li>
-            <li>Currently learning to play the Piano</li>
-            <li>I play the drums and occasionally sing</li>
-            <li>I like sports, music, activities, socialising, travelling and food!</li>
-          </ul>
+          <h4>FOOTBALL</h4>
+          <p>I currently play for Luton Allstarts FC & Nomura FC I play 3 times a week, I also enjoy watching football.</p>
+          <h4>MUSIC</h4>
+          <p>I play the drums and occasionally sing in a group. I am currently learning to play the Piano</p>
+          <h4>CARS</h4>
+          <p>I have organised meets for fellow young drivers to come together and present their projects</p>
+          <h4>ADRA AMBASSADOR</h4>
+          <p>Participate annually in the fundraising activities such as door-to-door collections and half marathon walks.</p>
+          <h4>OTHER INTERESTS</h4>
+          <p>I like sports, music, activities, socialising, travelling and food!</p>
           {close}
         </article>
 
