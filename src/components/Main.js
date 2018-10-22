0339164857ddfbfba28110723ea378a0237af1b4
Mickell <mickellc@hotmail.co.uk>
2018-10-22 09:42:52 +0100

    Add bullet list for qualifications in introduction component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
+import pic01 from '../images/lake_moraine.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
@@ -17,8 +17,26 @@ class Main extends React.Component {
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Introduction</h2>
           <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <p>
+            I started my career as an IT apprentice where I learned the basics of IT and gained some professional qualifications. These consisted of the following:
+          </p>
+          <ul>
+            <li>Windows 7 configuration (Microsoft Certified, MCTS)</li>
+            <li>CompTIA A+ IT Technician</li>
+            <li>Windows Operating Systems(Microsoft Certified)</li>
+            <li>Networking Fundamentals(Microsoft Certified)  Diploma in ICT Professional Competence (Distinction)</li>
+            <li>Diploma in ICT Systems & Principles (Distinction)</li>
+          </ul>
+          <p>
+          After completing my apprenticeship I then secured a field engineer role at Partnership Education where I delivered 1st/2nd line support to schools in Bedfordshire, Hertfordshire & Buckinghamshire.
+          The tasks I undertook included server support, setting up IT suites, Systems etc. I also achieved the Active Directory (Microsoft Certified, MCSA) & Microsoft Exchange 2010 (Microsoft Certified, MCITP) whilst at Partnership Education.
+          </p>
+
+          <p>
+          After being in my role for 2 years I made the decision to go and study, I enrolled on a computer science degree at Birkbeck University of London and carried on working at Partnership for the first 2 years of my degree.
+          After 3 years at Partnership I left my role and completed an internship with J.P.Morgan as a technology analyst, this was a great experience where I learnt a lot and made a great network. After this I decided to focus on my final year at university,
+          after I completed my degree I joined AND Digital. I am now an associate developer at AND Digital.
+          </p>
           {close}
         </article>
 
