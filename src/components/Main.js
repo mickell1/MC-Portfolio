c23ef743b3e1ca9ce44454c9679454982ab33332
Mickell <mickellc@hotmail.co.uk>
2018-10-22 17:20:41 +0100

    Add Linkedin icon to contact article and reformat about article

--- src/components/Main.js
+++ src/components/Main.js
@@ -100,6 +100,7 @@ class Main extends React.Component {
             <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
             <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
             <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="#" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
           </ul>
           {close}
         </article>
