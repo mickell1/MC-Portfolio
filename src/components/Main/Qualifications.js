1b324971c2ff68c71630a094706045deefc14e96
Mickell <mickellc@hotmail.co.uk>
2018-10-28 15:24:24 +0000

    Add content to qualifications component

--- src/components/Main/Qualifications.js
+++ src/components/Main/Qualifications.js
@@ -12,7 +12,30 @@ class Qualifications extends React.Component {
       <article id="qualifications" className={`${this.props.article === 'qualifications' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
         <h2 className="major">Qualifications</h2>
         <span className="image main"><img src={quals} alt="" /></span>
-        <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+        <h4>BIRKBECK UNIVERSITY OF LONDON [2014 - 2017]</h4>
+        <p>BSc Computing (Computer Science): 2.2</p>
+
+        <h4 className="qualsTitle">MICROSOFT IT ACADEMY [2014 - 2014]</h4>
+        <p className="qualsList">Active Directory (Microsoft Certified, MCSA)</p>
+        <p className="qualsList">Microsoft Exchange 2010 (Microsoft Certified, MCITP)</p>
+
+        <h4 className="qualsTitle">PEARSON’S (ADVANCED APPRENTICESHIP FOR IT & TELECOMS PROFESSIONALS)[2011 - 2011]</h4>
+        <p>Windows Operating Systems (Microsoft Certified, MTA)</p>
+        <p>Networking Fundamentals (Microsoft Certified, MTA)</p>
+        <p>CompTIA A+ IT Technician</p>
+        <p>Windows 7 configuration (Microsoft Certified, MCTS)</p>
+        <p>Diploma in ICT Professional Competence (Distinction)</p>
+        <p>Diploma in ICT Systems & Principles (Distinction)</p>
+
+        <h4>BARNFIELD COLLEGE [2009 - 2010]</h4>
+        <p>BTEC Level 2: First Diploma IT(pass)</p>
+        <p>Adult Numeracy(pass)</p>
+        <p>Adult Literacy(pass)</p>
+        <p>OCR Level 2: IT(Merit x2, Pass x2)</p>
+
+        <h4>LEALANDS HIGH SCHOOL [2004 - 2009]</h4>
+        <p>GCSE: Maths; English; Physical Education</p>
+
         {close}
       </article>
     )
