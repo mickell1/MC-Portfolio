9d6a85b39007a55adbac2ac1751173d458989e28
Mickell <mickellc@hotmail.co.uk>
2018-12-27 19:24:22 +0000

    Add context and format About component, add work experience to Experience component and add css styling for both components

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -13,16 +13,22 @@ class About extends React.Component {
         <h2 className="major">About</h2>
         <span className="image main"><img src={pic03} alt="" /></span>
         <p>
-        Experienced full-stack developer. Strong knowledge of agile practices and principles and is PSM1 certified. 5 years experience working in the IT industry this includes: tech support, field engineer, finance, legacy systems and software development.
+          Experienced full-stack developer. Strong knowledge of agile practices and principles and is PSM1 certified. 5 years experience working in the IT industry this includes: tech support, field engineer, finance, legacy systems and software development.
         </p>
         <h4>FOOTBALL</h4>
-        <p>I currently play for Luton Allstarts FC & Nomura FC I play 3 times a week, I also enjoy watching football.</p>
-        <h4>MUSIC</h4>
-        <p>I play the drums and occasionally sing in a group. I am currently learning to play the Piano</p>
+        <p>
+          I currently play for Luton Allstarts FC & Nomura FC I play 3 times a week, I also enjoy watching football.
+          Previously played and captained my Sunday league team
+        </p>
+        <h4>MUSICIAN</h4>
+        <p>
+          Gospel drummer & singer as part of my church’s music department and have performed in front of congregations with 150+ people.
+          I am currently learning to play the Piano
+        </p>
         <h4>CARS</h4>
-        <p>I have organised meets for fellow young drivers to come together and present their projects</p>
+        <p>I have organised meets for fellow young drivers to come together and present their projects </p>
         <h4>ADRA AMBASSADOR</h4>
-        <p>Participate annually in the fundraising activities such as door-to-door collections and half marathon walks.</p>
+        <p>Participate in annual appeals incl. half marathons and door-to-door fundraising.</p>
         <h4>OTHER INTERESTS</h4>
         <p>I like sports, music, activities, socialising, travelling and food!</p>
         {close}
--- src/components/Main/Experience.js
+++ src/components/Main/Experience.js
@@ -12,8 +12,37 @@ class Experience extends React.Component {
       <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
         <h2 className="major">Work Experience</h2>
         <span className="image main"><img src={workExp} alt="" /></span>
-        <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+        <h4>AND DIGITAL [2017 - NOW]</h4>
+        <p>Key Responsibilities</p>
+        <ul>
+          <li>Full-stack Javascript Engineer & Consultant, use Agile methods to help build digital products, teams and systems.</li>
+          <li>External Clients: Konsentus (Fintech / Open Banking)</li> 
+          <li>Techstack: AWS, React, JavaScript, HTML, Node, Docker, & Mongo DB</li>
+        </ul>
+
+        <h4>J.P.MORGAN | TECHNOLOGY ANALYST [2016 - 2016]</h4>
+        <p>Key Responsibilities</p>
+        <ul>
+          <li>Designed and built a website portal that was accessible by all J.P.Morgan staff, which involved organising co-design sessions and utilised Sharepoint, HTML, CSS & Javascript</li>
+          <li>Led a group of fellow summer analysts in the production of a marketing video. Post production utilised Adobe Premier Pro & After Effects</li>
+          <li>Wrote a blog on the importance of user involvement in software design/development</li>
+        </ul>
+
+        <h4>PTS CONSULTING | PARTNERSHIP EDUCATION - SYSTEMS ENGINEER [2012 - 2016]</h4>
+        <p>Key Responsibilities</p>
+        <ul>
+          <li>Provided software, hardware and server support (windows 8/7/Vista/Mac OS X); server 2003/2008 support; printer/router/SmartBoard to various schools across Bedfordshire</li>
+          <li>Set up user profiles and reset user passwords using active directory</li>
+          <li>Built computers and laptops using imaging software</li>
+          <li>Desktop/laptop repair. This includes changing motherboards, CD drives, & other components</li>
+        </ul>
+
+        <h4>LUTON BOROUGH COUNCIL | PAYROLL CLERK [2012 - 2012]</h4>
+
+        <h4>WHSMITH | CUSTOMER SERVICE ASSISTANT [2011 - 2012]</h4>
+
+        <h4>PEARSON'S | IT APPRENTICE  [2011 - 2011]</h4>
+
         {close}
       </article>
     )
