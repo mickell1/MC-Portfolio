9314cf75db6ef1f66d653153c19d538ec5cb85df
Mickell <mickellc@hotmail.co.uk>
2019-01-01 15:04:17 +0000

    Justify text for introduction and about component and add context to work experience component

--- src/components/Main/Experience.js
+++ src/components/Main/Experience.js
@@ -16,8 +16,20 @@ class Experience extends React.Component {
         <p>Key Responsibilities</p>
         <ul>
           <li>Full-stack Javascript Engineer & Consultant, use Agile methods to help build digital products, teams and systems.</li>
+          <li>build out monolith application; microservices, updating microservice template & skeleton.</li>
+          <li>Back end development for PSD2 and Open Banking product that enables consent management and compliance</li>
+          <li>integrated various new web functionality such as filtering status, course list, creation of course form and styling.</li>
+          <li>Built web dashboard containing profiles detailing individual’s skills and experience.</li>
           <li>External Clients: Konsentus (Fintech / Open Banking)</li> 
           <li>Techstack: AWS, React, JavaScript, HTML, Node, Docker, & Mongo DB</li>
+        </ul>
+        
+        <p>Key Achievements</p>
+        <ul>
+          <li>Joined AND Digital and became professional scrum master certified, since achieving this certification I have been scrum master/developer for our development team.</li>
+          <li>Facilitation of stand ups, sprint refinements, sprint reviews, sprint retrospectives and sprint planning and other events</li>
+          <li>Build relationship with clients and stakeholders</li>
+          <li>Implemented new TDD/BDD testing format to make tests and documentation clearer</li>
         </ul>
 
         <h4>J.P.MORGAN | TECHNOLOGY ANALYST [2016 - 2016]</h4>
